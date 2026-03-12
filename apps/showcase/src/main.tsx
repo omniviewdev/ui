da0b2626cf632b52e28fa029e54b651bd658c8d0
Joshua Pare <joshuabpare@gmail.com>
2026-03-12 16:34:42 -0500

    feat(showcase): add global reset styles and pass typecheck

--- apps/showcase/src/main.tsx
+++ apps/showcase/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@omniview/base-ui';
 import '@omniview/base-ui/styles.css';
+import './global.css';
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(

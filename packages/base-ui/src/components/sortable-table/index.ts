5b4e6c700320c7bd1b5a79c163559c581491b81b
Joshua Pare <joshuabpare@gmail.com>
2026-03-13 17:15:59 -0500

    feat(sortable-table): add useSortableTable hook

---
+++ packages/base-ui/src/components/sortable-table/index.ts
@@ -0,0 +1,9 @@
+export { useSortableTable } from './useSortableTable';
+export type {
+  SortConfig,
+  SortableColumnDef,
+  UseSortableTableOptions,
+  UseSortableTableReturn,
+} from './useSortableTable';
+export { SortableHeader } from './SortableHeader';
+export type { SortableHeaderProps } from './SortableHeader';
